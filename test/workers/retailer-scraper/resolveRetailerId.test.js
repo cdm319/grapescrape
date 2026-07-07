149e8d88dedf060b12e2fa9e0faba83b44dec58f
Chris Matthews <chris@matthews.digital>
2026-07-07 14:13:02 +0100

    Use Vitest for scraper tests

--- test/workers/retailer-scraper/resolveRetailerId.test.js
+++ test/workers/retailer-scraper/resolveRetailerId.test.js
@@ -1,5 +1,5 @@
 import assert from "node:assert/strict";
-import { describe, it } from "node:test";
+import { describe, it } from "vitest";
 import { resolveRetailerId } from "../../../src/workers/retailer-scraper/resolveRetailerId.js";
 
 describe('resolveRetailerId', () => {
